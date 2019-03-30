e55a81f59f89fa492e971776a0e86cf2ef3f14f3
ismail ÇADIR <ismailcadir@gmail.com>
2019-03-30 12:01:13 +0300

    Category silmedüzeltildi

--- MyEvernote.BusinessLayer/CategoryManager.cs
+++ MyEvernote.BusinessLayer/CategoryManager.cs
@@ -6,5 +6,30 @@ namespace MyEvernote.BusinessLayer
     public class CategoryManager : ManagerBase<Category>
     {
 
+        public override int Delete(Category category)
+        {
+            NoteManager noteManager = new NoteManager();
+            LikedManager likedManager = new LikedManager();
+            CommentManager commentManager = new CommentManager();
+            //kategori ile ilişkili notların silinmesi gerekiyor.
+            foreach (Note note in category.Notes)
+            {
+                //note ile ilişkili like'ları sileleim
+                foreach (Liked like in note.Likes)
+                {
+                    likedManager.Delete(like);
+                }
+
+                //note ile ilişkişli commentleirn silinmesi
+                foreach (Comment comment in note.Comments)
+                {
+                    commentManager.Delete(comment);
+                }
+
+                noteManager.Delete(note);
+            }
+
+            return base.Delete(category);
+        }
     }
 }
