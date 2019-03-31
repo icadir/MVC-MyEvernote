e741a6971bc85ac317791f3e3e2bef48d1786c85
ismail ÇADIR <ismailcadir@gmail.com>
2019-03-31 17:29:30 +0300

    ajax  yorum ekleme silme vs ok

--- MyEvernote.WebApp/Controllers/CommentController.cs
+++ MyEvernote.WebApp/Controllers/CommentController.cs
@@ -9,7 +9,7 @@ namespace MyEvernote.WebApp.Controllers
 {
     public class CommentController : Controller
     {
-        NoteManager noteManager = new NoteManager();
+        private NoteManager noteManager = new NoteManager();
 
         private CommentManager commentManager = new CommentManager();
 
@@ -29,6 +29,48 @@ namespace MyEvernote.WebApp.Controllers
             }
 
             return PartialView("_PartialComments", note.Comments);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int? id, string text)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = commentManager.Find(x => x.Id == id);
+            if (comment == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            comment.Text = text;
+            if (commentManager.Update(comment) > 0)
+            {
+                return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = commentManager.Find(x => x.Id == id);
+            if (comment == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            if (commentManager.Delete(comment) > 0)
+            {
+                return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
         }
     }
 }
