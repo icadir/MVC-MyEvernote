55d6ce30491509afff648fc63777e382f3d24ca7
ismail ÇADIR <ismailcadir@gmail.com>
2019-03-31 21:45:33 +0300

    note likes ok

--- MyEvernote.WebApp/Controllers/NoteController.cs
+++ MyEvernote.WebApp/Controllers/NoteController.cs
@@ -1,6 +1,7 @@
 ﻿using MyEvernote.BusinessLayer;
 using MyEvernote.Entities;
 using MyEvernote.WebApp.Models;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
@@ -138,6 +139,61 @@ namespace MyEvernote.WebApp.Controllers
             Note note = noteManager.Find(x => x.Id == id);
             noteManager.Delete(note);
             return RedirectToAction("Index");
+        }
+
+        public ActionResult GetLiked(int[] ids)
+        {
+            if (CurrentSession.User != null)
+            {
+                List<int> likedNoteIds = likedManager.List(
+                    x => x.LikedUser.Id == CurrentSession.User.Id && ids.Contains(x.Note.Id)).Select(
+                    x => x.Note.Id).ToList();
+
+                return Json(new { result = likedNoteIds });
+            }
+            else
+            {
+                return Json(new { result = new List<int>() });
+            }
+        }
+
+        public ActionResult SetLikeState(int noteid, bool liked)
+        {
+            int res = 0;
+            Liked like = likedManager.Find(x => x.Note.Id == noteid && x.LikedUser.Id == CurrentSession.User.Id);
+            Note note = noteManager.Find(x => x.Id == noteid);
+
+            if (like != null && liked == false)
+            {
+                res = likedManager.Delete(like);
+            }
+            else if (like == null && liked == true)
+            {
+                res = likedManager.Insert(new Liked()
+                {
+                    LikedUser = CurrentSession.User,
+                    Note = note,
+                });
+            }
+
+            if (res > 0)
+            {
+                if (liked)
+                {
+                    note.LikeCount++;
+
+                }
+                else
+                {
+                    note.LikeCount--;
+                }
+                res = noteManager.Update(note);
+
+                return Json(new { hasError = false, errorMessage = string.Empty, result = note.LikeCount });
+            }
+
+            return Json(new { hasError = true, errorMessage = "Beğenme işlemi Gerçekleştirilemedi..", result = note.LikeCount });
+
         }
 
     }
